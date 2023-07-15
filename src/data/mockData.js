52a013d7b5ac882b6857c427c0e73560d2852bdb
Amir Fahimi <amir.fahimi.aviation@gmail.com>
2023-07-16 00:02:58 +0330

    add mockdata file

---
+++ src/data/mockData.js
@@ -0,0 +1,46 @@
+const projectData = [
+    {
+        "projectTitle": "Project 1",
+        "zoneCount": 12,
+        "remainingDays": 5,
+        "id": 1
+    },
+    {
+        "projectTitle": "Project 2",
+        "zoneCount": 8,
+        "remainingDays": 0,
+        "id": 2
+    },
+    {
+        "projectTitle": "Project 3",
+        "zoneCount": 15,
+        "remainingDays": 122,
+        "id": 3
+    },
+    {
+        "projectTitle": "Project 4",
+        "zoneCount": 12,
+        "remainingDays": 5,
+        "id": 4
+    },
+    {
+        "projectTitle": "Project 5",
+        "zoneCount": 12,
+        "remainingDays": 5,
+        "id": 5
+    },
+    {
+        "projectTitle": "Project 6",
+        "zoneCount": 12,
+        "remainingDays": 5,
+        "id": 6
+    },
+    {
+        "projectTitle": "Project 7",
+        "zoneCount": 12,
+        "remainingDays": 5,
+        "id": 7
+    }
+];
+
+export default projectData
