2acfed1232cae7af02bab9452b7522e879b48f1e
Amir Fahimi <amir.fahimi.aviation@gmail.com>
2023-07-16 00:02:04 +0330

    add rout for projects

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/Login'
+import { Route, Routes } from 'react-router-dom'
+import Projects from './components/Projects'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Login />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/projects' element={<Projects />} />
+        {/* <Route path='/add-new-project' element={<New_project />} />
+        <Route path='/users' element={<Users />} /> */}
+      </Routes>
+
     </>
   )
 }
